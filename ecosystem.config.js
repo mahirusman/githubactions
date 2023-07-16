9444dcbdd118da52b537dbfc38fba7b16b770e44
muhammad usman <m.usman@renesistech.com>
2023-07-16 21:17:20 +0500

    safsadf

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-nodejs-app",
+      script: "app.js", // Replace 'app.js' with the entry point of your application
+      instances: "max", // You can specify the number of instances you want to run
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development", // Environment variables for development
+      },
+      env_production: {
+        NODE_ENV: "production", // Environment variables for production
+      },
+    },
+  ],
+};
