c603024f8e374c34b1a12f5ca01b10180a409dd8
muhammad usman <m.usman@renesistech.com>
2023-07-16 23:20:12 +0500

    asdsaf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "my-nodejs-app",
+      name: `my-nodejs-app ${process.env.NODE_ENV}`,
       script: "index.js", // Replace 'app.js' with the entry point of your application
       instances: "max", // You can specify the number of instances you want to run
       autorestart: true,
