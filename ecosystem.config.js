f0b0159664960a4b6b0ed842935e41c50245502c
muhammad usman <m.usman@renesistech.com>
2023-07-17 00:18:59 +0500

    asdfasgsg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,9 @@
+const NEV = process.env.NODE_ENV;
+
 module.exports = {
   apps: [
     {
-      name: `my-nodejs-${process.env.NODE_ENV}-app`,
+      name: `my-nodejs-${NEV}-app`,
       script: "index.js", // Replace 'app.js' with the entry point of your application
       instances: "max", // You can specify the number of instances you want to run
       autorestart: true,
