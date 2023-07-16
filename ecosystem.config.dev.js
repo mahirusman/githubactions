0cd11d7cd7b25974d28a9f290424a690c4fabf84
muhammad usman <m.usman@renesistech.com>
2023-07-16 23:34:37 +0500

    adding new changes

--- ecosystem.config.dev.js
+++ ecosystem.config.dev.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: `my-nodejs-prod-app`,
+      name: `my-nodejs-dev-app`,
       script: "index.js", // Replace 'app.js' with the entry point of your application
       instances: "max", // You can specify the number of instances you want to run
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
       env: {
-        NODE_ENV: "production", // Environment variables for development
+        NODE_ENV: "development", // Environment variables for development
       },
     },
   ],
