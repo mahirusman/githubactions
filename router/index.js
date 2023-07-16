df3d03821f5c7a309e66544b5729971b827eddb6
muhammad usman <m.usman@renesistech.com>
2023-07-16 20:53:14 +0500

    adding new changes

--- router/index.js
+++ router/index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post("/", createRecord);
 router.get("/", (req, res) => {
-  res.send(`<h1>Working perfect</h1>`);
+  res.send(`<h1>Working dev Env</h1>`);
 });
 router.get("/", getRecordsList);
 router.get("/find", getRecordsFindList);
