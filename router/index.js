bb069f37b1935206e4b3fbe9f20e22f9a1cd0322
muhammad usman <m.usman@renesistech.com>
2023-07-16 18:29:37 +0500

    asdfsfsdfs

--- router/index.js
+++ router/index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post("/", createRecord);
 router.get("/", (req, res) => {
-  res.send("asdfsfsd");
+  res.send(<h1>Working perfect</h1>);
 });
 
 router.get("/", getRecordsList);
