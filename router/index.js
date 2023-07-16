ce2d841d10dfd7e34f6e9eee90f5c0129ad14ec0
muhammad usman <m.usman@renesistech.com>
2023-07-16 22:46:31 +0500

    sdfsafdsfs

--- router/index.js
+++ router/index.js
@@ -13,7 +13,11 @@ router.post("/", createRecord);
 
 router.get("/", (req, res) => {
   return res.status(200).json({
-    message: "this is our",
+    Hi: "hell from Dev environment",
+    server: `this app is using aws EC2 ubuntu server`,
+    Nginx: `using Nginx for revers proxy instead of Apache`,
+    SSL: "using free version of SSL certbot",
+    CICD: "using gitHUb Actions for CI/CD on self hosted container",
   });
 });
 // router.get("/", getRecordsList);
