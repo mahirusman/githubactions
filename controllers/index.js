7f68c78a8eb46fe92e5132cc417be39ec766ab25
muhammad usman <m.usman@renesistech.com>
2023-07-16 12:36:10 +0500

    adding new changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,74 @@
 const postModel = require("../models/post");
 
+const getRecordsList = async (req, res) => {
+  let {
+    page = 1,
+    limit = 10,
+    sortField = "createdAt",
+    order = "desc",
+    search = "",
+  } = req.query;
+  let sortOrder = order == "asc" ? 1 : -1;
+  let skip = (+page - 1) * +limit;
+  limit = +limit;
+
+  const results = await postModel.find({
+    title: { $regex: search, $options: "i" },
+  });
+
+  res.status(200).json({
+    results,
+    page,
+    limit,
+  });
+};
+
+const getRecordsFindList = async (req, res) => {
+  let {
+    page = 1,
+    limit = 10,
+    sortField = "createdAt",
+    order = 1,
+    search = "",
+  } = req.query;
+  console.log("req.query", req.query);
+
+  const query = {};
+
+  // Apply search filter if provided
+  if (search) {
+    query["text"] = { $regex: search, $options: "i" };
+  }
+
+  const pipeline = [
+    { $match: query },
+    {
+      $facet: {
+        data: [
+          { $sort: { [sortField]: parseInt(order) } },
+          { $skip: (page - 1) * limit },
+          { $limit: parseInt(limit) },
+        ],
+        totalCount: [{ $count: "count" }],
+      },
+    },
+  ];
+
+  const result = (await postModel.aggregate(pipeline))[0] ?? [];
+
+  const results = result?.data;
+  const totalCount = result.totalCount[0] ? result.totalCount[0].count : 0;
+  const totalPages = totalCount / limit;
+  res.status(200).json({
+    results,
+    page,
+    limit,
+    totalCount,
+    totalPages: totalPages,
+    hasMore: totalPages > page,
+  });
+};
+
 const createRecord = async (req, res) => {
   const { title, description, tags } = req.body;
   const results = await postModel.create({
@@ -40,4 +109,10 @@ const deletePost = async (req, res) => {
   }
 };
 
-module.exports = { createRecord, updateRecord, deletePost };
+module.exports = {
+  createRecord,
+  updateRecord,
+  deletePost,
+  getRecordsList,
+  getRecordsFindList,
+};
--- router/index.js
+++ router/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { createRecord, updateRecord, deletePost } = require("../controllers");
+const {
+  createRecord,
+  updateRecord,
+  deletePost,
+  getRecordsList,
+  getRecordsFindList,
+} = require("../controllers");
 
 const router = express.Router();
 
 router.post("/", createRecord);
+
+router.get("/", getRecordsList);
+router.get("/find", getRecordsFindList);
 
 router.put("/:id", updateRecord);
 router.delete("/:id", deletePost);
